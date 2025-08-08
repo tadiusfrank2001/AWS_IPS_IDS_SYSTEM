8fc694b85649e94ceab0422e18a4a55587b21063
Tadius Frank <tadiusfrank2001@gmail.com>
2025-08-08 01:15:48 -0400

    Data Sources for VPC type, subnet ID, AMI's for EC2's, current region and caller identity for dynamic deployment

--- main.tf
+++ main.tf
@@ -30,3 +30,55 @@ resource "random_id" "lab_suffix" {
   byte_length = 4
 }
 
+
+
+
+
+
+# ===== DATA SOURCES =====
+
+# This allows you to place your EC2s into existing network infrastructure without having to create VPCs/subnets manually.
+# Fetches your default VPC, we could customise our VPC but it's not neccessary right now
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Gets all subnets in that default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# Get Amazon Linux 2 AMI
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+# Get Amazon Linux 2023 AMI
+data "aws_ami" "amazon_linux_2023" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+# Get current AWS region and account, best practice is to avoid hardcoding these fields
+# Because we are getting the current identity and region we can deploy this code and build anywhere
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+
+
+
+
