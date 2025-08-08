4872e7aac46e2578cb1f889cc370f08e47479d86
Tadius Frank <tadiusfrank2001@gmail.com>
2025-08-08 05:04:26 -0400

    Ensure Terraform has credentials to use AWS resources on an account

--- main.tf
+++ main.tf
@@ -22,7 +22,9 @@ terraform {
 
 # Configure AWS Provider
 provider "aws" {
-  region = var.region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.region
 }
 
 # Generate random suffix for unique naming of S3 buckets
--- variables.tf
+++ variables.tf
@@ -58,5 +58,20 @@ variable "alert_email" {
 
 
 
+# AWS CREDENTIALS
+
+# NOTE: The "true" variable in sensitive makes sure this info those not appear in logs!
+variable "aws_access_key" {
+  description = "AWS Access Key ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Access Key" 
+  type        = string
+  sensitive   = true
+}
+
 
 
