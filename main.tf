64a5298c1454f2509fbb63265b9087642740aa6f
Tadius Frank <tadiusfrank2001@gmail.com>
2025-08-08 04:54:33 -0400

    Eventbridge: rule to trigger Lambda when GuardDuty finds threatsthen invoke the EventBridge

--- main.tf
+++ main.tf
@@ -458,6 +458,8 @@ resource "aws_securityhub_account" "gd_lab_security_hub" {
 
 
 
+
+
 # ===== SNS FOR ALERTS =====
 
 # SNS topic for sending security alerts
@@ -554,6 +556,56 @@ resource "aws_lambda_function" "gd_stop_compromised_instance" {
     Name = "GuardDuty Automated Response"
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+# ===== EVENTBRIDGE RULE =====
+
+# EventBridge rule to trigger Lambda when GuardDuty finds threats
+resource "aws_cloudwatch_event_rule" "gd_guardduty_rule" {
+  name        = "gd-guardduty-rule-${random_id.lab_suffix.hex}"
+  description = "Trigger automated response on GuardDuty findings"
+
+  event_pattern = jsonencode({
+    source      = ["aws.guardduty"]
+    detail-type = ["GuardDuty Finding"]
+    detail = {
+      severity = [4.0, 4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8, 4.9, 5.0, 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9, 6.0, 6.1, 6.2, 6.3, 6.4, 6.5, 6.6, 6.7, 6.8, 6.9, 7.0, 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 8.0, 8.1, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 8.8, 8.9, 9.0, 9.1, 9.2, 9.3, 9.4, 9.5, 9.6, 9.7, 9.8, 9.9, 10.0]
+    }
+  })
+
+  tags = {
+    Name = "GuardDuty EventBridge Rule"
+  }
+}
+
+
+# Target Lambda function from EventBridge rule
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule      = aws_cloudwatch_event_rule.gd_guardduty_rule.name
+  target_id = "GuardDutyLambdaTarget"
+  arn       = aws_lambda_function.gd_stop_compromised_instance.arn
+}
+
+# Allow EventBridge to invoke the Lambda function
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.gd_stop_compromised_instance.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.gd_guardduty_rule.arn
+}
+
+
 
 
 
