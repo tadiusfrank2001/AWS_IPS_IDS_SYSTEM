59cd81c950e1dee872ff129d2095e9d75041a02d
Tadius Frank <tadiusfrank2001@gmail.com>
2025-08-14 03:33:01 -0400

    S3 Bucket and SSH commands to connect to compromised and attacker instances

--- outputs.tf
+++ outputs.tf
@@ -30,3 +30,46 @@ output "malicious_instance_private_ip" {
   description = "Private IP address of the malicious instance"
   value       = aws_instance.ec2_malicious.private_ip
 }
+
+
+
+
+
+
+# ===== SSH CONNECTION COMMANDS =====
+
+output "ssh_command_compromised" {
+  description = "SSH command to connect to the compromised instance"
+  value       = "ssh -i ~/.ssh/guardduty_lab_key ec2-user@${aws_instance.ec2_compromised.public_ip}"
+}
+
+output "ssh_command_malicious" {
+  description = "SSH command to connect to the malicious instance"
+  value       = "ssh -i ~/.ssh/guardduty_lab_key ec2-user@${aws_eip.malicious_ip.public_ip}"
+}
+
+
+
+
+
+
+
+
+# ===== S3 BUCKET OUTPUTS =====
+
+output "guardduty_bucket_name" {
+  description = "Name of the GuardDuty lab S3 bucket"
+  value       = aws_s3_bucket.gd_lab_bucket.bucket
+}
+
+output "threat_list_bucket_name" {
+  description = "Name of the threat intelligence S3 bucket"
+  value       = aws_s3_bucket.gd_threat_list.bucket
+}
+
+output "threat_list_url" {
+  description = "S3 URL of the threat intelligence list"
+  value       = "https://${aws_s3_bucket.gd_threat_list.bucket}.s3.${data.aws_region.current.name}.amazonaws.com/threatlist.txt"
+}
+
+
